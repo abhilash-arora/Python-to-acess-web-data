1d0cb771469c80a0038eb6ddcd566019ef9c2ce7
abhilash-arora <63860328+abhilash-arora@users.noreply.github.com>
2020-07-02 00:15:46 +0530

    Add files via upload

---
+++ sumassignment.py
@@ -0,0 +1,27 @@
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl,re
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+
+
+sum=0
+# Retrieve all of the span tags
+tags = soup('span')
+for tag in tags:
+    # Look at the parts of a tag
+    y=str(tag)
+    x= re.findall("[0-9]+",y)
+    for i in x:
+        i=int(i)
+        sum=sum+i
+print(sum)
